df770de2489fd864b4961bd704fad43e2f2b74ca
Abhiit-666 <93072878+Abhiit-666@users.noreply.github.com>
2021-11-21 15:58:14 +0530

    Update add.py

--- add.py
+++ add.py
@@ -2,12 +2,5 @@ def add(x,y):
 	print("okay!!")
 	return x + y
 
-num1 = 1.5
-num2 = 6.3
 
-# Add two numbers
-sum = num1 + num2
-
-# Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 
