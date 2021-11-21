ddab6541bab977dff53d88a0376f93e345752942
Abhiit-666 <93072878+Abhiit-666@users.noreply.github.com>
2021-11-21 16:11:22 +0530

    Update addtest.py

--- addtest.py
+++ addtest.py
@@ -4,7 +4,7 @@ import add
 class TestCalc(unittest.TestCase):
 	def test_add(self):
 		result=add.add(10,5)
-		self.assertEqual(result,15)
+		self.assertEqual(result,5)
 
 if __name__ =='__main__':
 	unittest.main()
